5d512debdedf7c56f54241589408dcd9cb4c266e
Michael Vetter <g.bluehut@gmail.com>
2013-07-16 22:32:52 +0200

    adding script to download plugins

---
+++ get_scripts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+set +v
+
+function download_script()
+{
+	echo $1
+	echo $2
+	wget $1 -O ~/.irssi/scripts/autorun/$2
+}
+
+mkdir -p ~/.irssi/scripts/autorun
+#colour all nicks and remember the colour for each
+download_script http://scripts.irssi.org/scripts/nickcolor.pl nickcolor.pl
+#extra window for hilights
+download_script http://scripts.irssi.org/scripts/hilightwin.pl hilightwin.pl
+#a better windowlist
+download_script http://anti.teamidiot.de/static/nei/*/Code/Irssi/adv_windowlist.pl adv_windowlist.pl
+#shows where you stopped reading
+download_script http://scripts.irssi.org/scripts/trackbar.pl trackbar.pl
+#auto whois people who pm you
+download_script http://scripts.irssi.org/scripts/auto_whois.pl auto_whois.pl
+#nicklist
+download_script http://scripts.irssi.org/scripts/nicklist.pl nicklist.pl
